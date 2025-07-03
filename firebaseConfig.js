3800fa3a4f597f1fec294f635d13d1870636c763
LowCreator <vinicius.oliveira.99@hotmail.com>
2025-07-03 02:02:41 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,22 @@
 // firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-// 🔐 substitua pelos dados do seu Firebase
+// 🔐 Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBk4DEhpSsakwZdumaWS6AO0Lre2CPuicU",
   authDomain: "contafacil-ca62a.firebaseapp.com",
   projectId: "contafacil-ca62a",
-  storageBucket: "contafacil-ca62a.firebasestorage.app",
+  storageBucket: "contafacil-ca62a.appspot.com",
   messagingSenderId: "515706577788",
   appId: "1:515706577788:web:d2425fd6a74ce81acc3af9",
   measurementId: "G-L9Z5T75H9K"
 };
 
-
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta Firestore e Auth
 export const db = getFirestore(app);
+export const auth = getAuth(app);
